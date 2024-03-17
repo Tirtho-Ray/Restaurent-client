2108bf3b308211822b2450cdf601a869edbd3945
Tirtho-Ray <tirthoray10@gmail.com>
2024-03-18 00:01:38 +0600

    restaurent

--- src/components/Footer/Order.jsx
+++ src/components/Footer/Order.jsx
@@ -6,7 +6,7 @@ const Order = () => {
             <h1 className="text-sm md:text-xl font-bold mt-3">How To Order</h1>
           <div className="mt-3">
             <p className="font-bold text-sm">How to place an order.</p>
-                <p className="font-bold text-sm">How to place a DFH order.</p>
+               
           </div>
             
         </div>
